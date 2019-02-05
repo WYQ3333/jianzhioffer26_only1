c242fecab77e316f5f2be965cee17cc99bcb9302
浅忆梦微凉 <3538922590@qq.com>
2019-02-05 18:57:25 +0800

    编译通过

---
+++ 剑指offer26_整数中1出现的次数/test.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int NumberOf1Between1AndN_Solution(int n)
+	{
+		int i = 0;
+		int result = 1;
+		if (n < 10){
+			return 1;
+		}
+		for (i = 10; i < n; ++i){
+			while (i){
+				if (i % 10 == 1){
+					++result;
+				}
+				i = i / 10;
+			}
+		}
+		return result;
+	}
+};
+
+void TestFunc(){
+
+}
+
+int main(){
+	TestFunc();
+	system("pause");
+	return 0;
+}
